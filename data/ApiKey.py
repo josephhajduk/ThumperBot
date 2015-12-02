5e416b342342c7d074cf44cd15ed129bf4f5d0d2
Joseph Hajduk <joseph@solidys.com>
2015-12-02 04:13:07 -0700

    added placeholders

--- data/ApiKey.py
+++
@@ -1,8 +0,0 @@
-
-
-class ApiKey(BaseModel):
-    user = ForeignKeyField(User)
-    key_id = IntegerField(unique=True)
-    verification_code = TextField()
-    last_queried = DateTimeField(default=datetime.datetime.min)
-    mask = TextField(default="")
