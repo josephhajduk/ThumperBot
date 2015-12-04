020e15915ed424dc6e45a273760e81094e94a2a6
Joseph Hajduk <joseph@solidys.com>
2015-12-03 17:50:57 -0700

    apipull using tasks to handle blocking eveapi

--- apipull/apipull.py
+++ apipull/apipull.py
@@ -24,113 +24,136 @@ def get_key_details(key_id, verification_code):
     except:
         return None
 
-#TODO:  if somebody modifies their main character while the api is running this will POOOP OUT
+@asyncio.coroutine
+def run_key(bot,apikey):
+    try:
+        eveapi.set_user_agent("eveapi.py/1.3")
+        api = eveapi.EVEAPIConnection()
+        key_id = apikey.key_id
+        code = apikey.verification_code
+        apikey_user = apikey.user
+        mainname = "NOMAIN"
+        if apikey_user.main_character is not None:
+            mainname= apikey_user.main_character.name
+        invalid = False
+
+        auth = api.auth(keyID=key_id, vCode=code)
+
+        minimal_mask = 16777216
+        result1 = auth.account.APIKeyInfo().key
+
+        if not result1.accessMask & minimal_mask:
+            logging.error("INVALID MASK KEY: " + str(
+                key_id) + " : " + code + " for " + mainname)
+            invalid = True
+
+        apikey.mask = result1.accessMask
+
+        if result1.expires != "":
+            logging.error("EXPIRING KEY: " + str(
+                key_id) + " : " + code + " for " + mainname)
+            invalid = True
+
+        if result1.type != "Account":
+            logging.error("WRONG TYPE KEY: " + str(
+                key_id) + " : " + code + " for " + mainname)
+            invalid = True
+
+        if not invalid:
+            result2 = auth.account.Characters()
+
+            charstring = ""
+
+            for character in result2.characters:
+
+                charInfo = auth.eve.CharacterInfo(characterID=character.characterID)
+
+                print(character.name)
+
+                #what happens when char changes corp or alliance?
+
+                location = ""
+                if hasattr(charInfo,"lastKnownLocation"):
+                    location = charInfo.lastKnownLocation
+
+                if hasattr(charInfo, "alliance"):
+                    dbchar, created = Character.get_or_create(
+                        user=apikey_user,
+                        name=character.name,
+                        eve_id=character.characterID,
+                        corporation_name=charInfo.corporation,
+                        corporation_id=charInfo.corporationID,
+                        alliance_name=charInfo.alliance,
+                        alliance_id=charInfo.allianceID,
+                        shiptype_name=charInfo.shipTypeName,
+                        shiptype_id=charInfo.shipTypeID,
+                        location=location,
+                        api_key=apikey)
+                else:
+                    dbchar, created = Character.get_or_create(
+                        user=apikey_user,
+                        name=character.name,
+                        eve_id=character.characterID,
+                        corporation_name=charInfo.corporation,
+                        corporation_id=charInfo.corporationID,
+                        shiptype_name=charInfo.shipTypeName,
+                        shiptype_id=charInfo.shipTypeID,
+                        location=location,
+                        api_key=apikey)
+
+                charstring += "  " + character.name + "\n"
+    except:
+        #invalid = True
+        #TODO: DETECT DELETED KEY
+        logging.error("API ERROR: BAD KEY: " + str(
+            key_id) + " : " + code + " for " + mainname)
+        traceback.print_exc()
+        print("API ERROR: BAD KEY: " + str(
+            key_id) + " : " + code + " for " + mainname)
+
+
+    apikey.last_queried = datetime.datetime.now()
+    apikey.invalid = invalid
+    apikey.save()
+
+    try:
+
+        if invalid:
+            logging.info("Purging characters from key: " + str(key_id))
+            for delcharacter in Character.select().where(Character.api_key == apikey):
+                delcharacter.delete_instance(recursive=True)
+
+            yield from bot.sendMessage(apikey_user.telegram_id, "API KEY: " + str(
+                key_id) + " has been made invalid,  your characters on this key have been purged.  Please readd a new proper key")
+
+        if apikey_user.main_character is None:
+            if not invalid:
+                yield from bot.sendMessage(apikey_user.telegram_id, _s["msg_keysrun"])
+
+    except:
+        print("FAIL1")
+        traceback.print_exc()
+    pass
+
 
 async def check_api_loop(bot, loop):
     while True:
         try:
-            # Provide a good User-Agent header
-            eveapi.set_user_agent("eveapi.py/1.3")
-            api = eveapi.EVEAPIConnection()
+            tasks = [run_key(bot, apikey) for apikey in ApiKey.select().where(
+                ApiKey.last_queried < datetime.datetime.now() - datetime.timedelta(hours=8)
+            )]
 
-            print(len(ApiKey.select().where(
-                (ApiKey.last_queried < datetime.datetime.now() - datetime.timedelta(hours=8)))))
+            print("Running "+str(len(tasks))+" api keys...")
 
-            for apikey in ApiKey.select().where(
-                    (ApiKey.last_queried < datetime.datetime.now() - datetime.timedelta(hours=8))):
-                mainname = "nomainset"
-                if apikey.user.main_character is not None:
-                    mainname= apikey.user.main_character.name
-                invalid = False
+            def chunks(l, n):
+                for i in range(0, len(l), n):
+                    yield l[i:i+n]
 
-                try:
-                    auth = api.auth(keyID=apikey.key_id, vCode=apikey.verification_code)
+            #limit to 15 per second
 
-                    minimal_mask = 16777216
-                    result1 = auth.account.APIKeyInfo().key
-
-                    if not result1.accessMask & minimal_mask:
-                        logging.error("INVALID MASK KEY: "+str(apikey.key_id)+" : "+apikey.verification_code+" for "+mainname)
-                        invalid = True
-
-                    apikey.mask = result1.accessMask
-
-                    if result1.expires != "":
-                        logging.error("EXPIRING KEY: "+str(apikey.key_id)+" : "+apikey.verification_code+" for "+mainname)
-                        invalid = True
-
-                    if result1.type != "Account":
-                        logging.error("WRONG TYPE KEY: "+str(apikey.key_id)+" : "+apikey.verification_code+" for "+mainname)
-                        invalid = True
-
-                    if not invalid:
-                        result2 = auth.account.Characters()
-
-                        charstring = ""
-
-                        for character in result2.characters:
-
-                            charInfo = auth.eve.CharacterInfo(characterID=character.characterID)
-
-                            print(character.name)
-
-                            location = ""
-                            if hasattr(charInfo,"lastKnownLocation"):
-                                location = charInfo.lastKnownLocation
-
-                            if hasattr(charInfo, "alliance"):
-                                dbchar, created = Character.get_or_create(
-                                    user=apikey.user,
-                                    name=character.name,
-                                    eve_id=character.characterID,
-                                    corporation_name=charInfo.corporation,
-                                    corporation_id=charInfo.corporationID,
-                                    alliance_name=charInfo.alliance,
-                                    alliance_id=charInfo.allianceID,
-                                    shiptype_name=charInfo.shipTypeName,
-                                    shiptype_id=charInfo.shipTypeID,
-                                    location=location,
-                                    api_key=apikey)
-                            else:
-                                dbchar, created = Character.get_or_create(
-                                    user=apikey.user,
-                                    name=character.name,
-                                    eve_id=character.characterID,
-                                    corporation_name=charInfo.corporation,
-                                    corporation_id=charInfo.corporationID,
-                                    shiptype_name=charInfo.shipTypeName,
-                                    shiptype_id=charInfo.shipTypeID,
-                                    location=location,
-                                    api_key=apikey)
-
-                            charstring += "  " + character.name + "\n"
-                except:
-                    #invalid = True
-                    #TODO: DETECT DELETED KEY
-                    logging.error("API ERROR: BAD KEY: "+str(apikey.key_id)+" : "+apikey.verification_code+" for "+mainname)
-
-
-                apikey.last_queried = datetime.datetime.now()
-                apikey.invalid = invalid
-                apikey.save()
-
-                try:
-
-                    if invalid:
-                        logging.info("Purging characters from key: "+str(apikey.key_id))
-                        for delcharacter in Character.select().where(Character.api_key == apikey):
-                            delcharacter.delete_instance(recursive=True)
-
-                        await bot.sendMessage(apikey.user.telegram_id, "API KEY: "+str(apikey.key_id)+" has been made invalid,  your characters on this key have been purged.  Please readd a new proper key")
-
-
-                    if apikey.user.main_character is None:
-                        if not invalid:
-                            await bot.sendMessage(apikey.user.telegram_id, _s["msg_keysrun"])
-
-                except:
-                    print("FAIL1")
-                    traceback.print_exc()
+            for chunk in chunks(tasks, 15):
+                await asyncio.gather(*chunk)
+                await asyncio.sleep(1)
 
             await asyncio.sleep(60 * 3)  # run apis every 3 minutes
         except:
