3be2325fb7052eea72d8fc82c3d2cbef845092bb
Joseph Hajduk <joseph@solidys.com>
2015-12-13 01:58:46 -0600

    reply messages in debug output

--- ThumperBot.py
+++ ThumperBot.py
@@ -12,12 +12,20 @@ from web import pingforward_web_init
 
 from botdata import get_config_item
 
-
-TOKEN = "137055148:AAHOCCRyHsqlkcSZR1EyuSQxLVn76aYXirQ"
 TOKEN2 = "170018274:AAHJ_J3dGa4bQVyHk5KNXtITswr-2sfk2dM"
 TOKEN3 = "154397353:AAEChVXNz7BXxenwjNokfryfZqUgiwZLN6A"
 
-bot = telepot.async.DelegatorBot(get_config_item("TELEPOT_TOKEN", TOKEN3), [
+
+class LoggedDelegatorBot(telepot.async.DelegatorBot):
+    def __init__(self, token, delegation_patterns, loop=None):
+        super(LoggedDelegatorBot, self).__init__(token, delegation_patterns, loop)
+
+    @asyncio.coroutine
+    def sendMessage(self, chat_id, text, parse_mode=None, disable_web_page_preview=None, reply_to_message_id=None, reply_markup=None):
+        print("[REPLY TO: "+str(chat_id)+"]:\n"+str(text))
+        yield from super(LoggedDelegatorBot, self).sendMessage(chat_id, text, parse_mode, disable_web_page_preview, reply_to_message_id, reply_markup)
+
+bot = LoggedDelegatorBot(get_config_item("TELEPOT_TOKEN", TOKEN3), [
     (per_chat_id(), create_open(ConversationHandler, timeout=get_config_item("TELEPOT_CONVO_TIMEOUT", 60*10))),
 ])
 
